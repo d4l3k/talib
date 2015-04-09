1ad872508b2ca5be9e86eade414ba5100fa76913
Tristan Rice <rice@fn.lc>
2015-04-08 19:42:58 -0700

    Added test for non slice inputs and multiple outputs

--- talib_test.go
+++ talib_test.go
@@ -24,3 +24,19 @@ func TestSin(t *testing.T) {
 		t.Errorf("Expected %#v got %#v.", expected, out)
 	}
 }
+func TestMacd(t *testing.T) {
+	data := []float64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60}
+	fast, slow, signal := talib.Macd(data, 12, 26, 9)
+	expectedFast := []float64{7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	expectedSlow := []float64{7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	expectedSignal := []float64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	if !reflect.DeepEqual(expectedFast, fast) {
+		t.Errorf("Expected %#v got %#v.", expectedFast, fast)
+	}
+	if !reflect.DeepEqual(expectedSlow, slow) {
+		t.Errorf("Expected %#v got %#v.", expectedSlow, slow)
+	}
+	if !reflect.DeepEqual(expectedSignal, signal) {
+		t.Errorf("Expected %#v got %#v.", expectedSignal, signal)
+	}
+}
